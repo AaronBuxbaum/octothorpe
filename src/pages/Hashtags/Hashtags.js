2e594e2894cb3897b83d1f735740257b4c9c57de
Aaron Buxbaum <me@aaronbuxbaum.com>
2018-10-15 22:23:49 -0400

    Move shit around

--- src/pages/Hashtags/Hashtags.js
+++ src/pages/Hashtags/Hashtags.js
@@ -10,6 +10,7 @@ import Footer from 'grommet/components/Footer';
 import SelectedHashtags from './SelectedHashtags';
 import { firestoreConnect, withFirestore } from 'react-redux-firebase';
 import { connect } from 'react-redux';
+import { renderWhileLoading } from '../../hoc/loading';
 
 const MAX_SUGGESTIONS = 5;
 const suggestions = [
@@ -17,57 +18,60 @@ const suggestions = [
     'more things',
     'scuba diving',
 ];
-const popularities = {
+const popularitiesMap = {
     'things': 1,
     'more things': 3,
     'scuba diving': 9049,
 };
-const postfixes = [
-    { 'B': 1E9 },
-    { 'M': 1E6 },
-    { 'K': 1E3 },
-    { '': 1 }
-];
 
 class Hashtags extends React.Component {
     state = {
         isWorking: false,
         selected: [],
         suggestions: [],
+        popularities: {},
         value: '',
     };
 
-    formatPopularity = (num) => {
-        for (let i = 0; i < postfixes.length; i++) {
-            const postfix = postfixes[i];
-            const key = Object.keys(postfix)[0];
-            const value = postfix[key];
-            if (num >= value) {
-                return Math.round(num / value) + key;
-            }
-        }
-
-        return num;
-    };
-
-    getPopularity = (suggestion) => {
-        const popularity = popularities[suggestion] || 1;
-        return this.formatPopularity(popularity);
+    componentDidMount() {
+        const selected = this.props[hashtags].map((hashtag) => ({
+            title: hashtag.value,
+            popularity: 1,
+        }));
+        this.setState({
+            selected,
+        });
     };
 
     getSuggestions = (input) => {
         const startsWithInput = (suggestion) => suggestion.startsWith(input);
-        const relevantSuggestions = suggestions.filter(startsWithInput);
+        let relevantSuggestions = suggestions.filter(startsWithInput);
         if (relevantSuggestions[0] !== input)
             relevantSuggestions.unshift(input);
         return take(relevantSuggestions, MAX_SUGGESTIONS);
     };
 
+    getPopularities = (items) => {
+        const newPopularities = {};
+
+        items.forEach((item) => {
+            if (this.state.popularities[item]) {
+                return;
+            }
+
+            newPopularities[item] = popularitiesMap[item] || 1;
+        });
+
+        return { ...this.state.popularities, ...newPopularities };
+    };
+
     updateSuggestions = (event) => {
         const value = event.target.value;
         const suggestions = this.getSuggestions(value.trim());
+        const popularities = this.getPopularities(suggestions);
         this.setState({
             suggestions,
+            popularities,
             value,
         });
     };
@@ -92,9 +96,8 @@ class Hashtags extends React.Component {
             return;
         }
 
-        const popularity = this.getPopularity(title);
         const newSelectedItem = {
-            popularity,
+            popularity: this.state.popularities[title],
             title,
         };
 
@@ -128,7 +131,7 @@ class Hashtags extends React.Component {
         const db = this.props.firestore;
         const uid = this.props.uid;
 
-        db.collection('hashtags')
+        db.collection(hashtags)
             .where('uid', '==', uid)
             .get()
             .then((snapshot) => {
@@ -139,7 +142,7 @@ class Hashtags extends React.Component {
             .then(() =>
                 this.state.selected.forEach(({ title }) => {
                     return db.add(
-                        { collection: 'hashtags' },
+                        { collection: hashtags },
                         { value: title, uid },
                     );
                 })
@@ -181,7 +184,7 @@ const hashtags = 'hashtags';
 
 const mapStateToProps = (state) => ({
     uid: state.firebase.auth.uid,
-    [hashtags]: state.firestore.ordered.hashtags,
+    [hashtags]: state.firestore.ordered[hashtags],
 });
 
 export default compose(
@@ -189,11 +192,11 @@ export default compose(
     connect(mapStateToProps),
     firestoreConnect((props) => [
         {
-            collection: 'hashtags',
+            collection: hashtags,
             where: [
                 ['uid', '==', props.uid || 0]
             ],
-            storeAs: hashtags,
         }
     ]),
+    renderWhileLoading([hashtags]),
 )(Hashtags);
--- src/pages/Hashtags/SelectedHashtags/Row.js
+++ src/pages/Hashtags/SelectedHashtags/Row.js
@@ -2,6 +2,26 @@ import React from "react";
 import ListItem from "grommet/components/ListItem";
 import CloseButton from "./CloseButton";
 
+const postfixes = [
+    { 'B': 1E9 },
+    { 'M': 1E6 },
+    { 'K': 1E3 },
+    { '': 1 }
+];
+
+const formatPopularity = (num) => {
+    for (let i = 0; i < postfixes.length; i++) {
+        const postfix = postfixes[i];
+        const key = Object.keys(postfix)[0];
+        const value = postfix[key];
+        if (num >= value) {
+            return Math.round(num / value) + key;
+        }
+    }
+
+    return num;
+};
+
 const infoColumnStyle = {
     alignItems: "center",
     display: "flex",
@@ -16,7 +36,7 @@ const Row = ({
                 {item.title}
             </span>
             <span className="secondary" style={infoColumnStyle}>
-                {item.popularity}
+                {formatPopularity(item.popularity)}
                 <CloseButton onClick={removeItem} />
             </span>
         </ListItem>
