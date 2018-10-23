62a6f70e8f1444a3c024d0b030450d0a72c080a9
Aaron Buxbaum <me@aaronbuxbaum.com>
2018-10-22 23:56:48 -0400

    Refactor out get username utility

--- server/src/resolvers/mutations/authentication/index.ts
+++ server/src/resolvers/mutations/authentication/index.ts
@@ -1,11 +1,9 @@
 import login from './login';
 import signup from './signup';
-import getUser from './getUser';
 
 const authenticaton = {
     login,
     signup,
-    getUser,
 };
 
 export default authenticaton;
--- server/src/resolvers/mutations/utils/getUsername.ts
+++ server/src/resolvers/mutations/utils/getUsername.ts
@@ -1,15 +1,15 @@
 import { verify } from 'jsonwebtoken';
-import { APP_SECRET } from './constants';
+import { APP_SECRET } from '../authentication/constants';
 
-const getUser = (context) => {
+const getUsername = (context) => {
     const Authorization = context.request.get('Authorization');
     if (Authorization) {
         const token = Authorization.replace('Bearer ', '');
-        const { userId } = verify(token, APP_SECRET);
-        return userId;
+        const { username } = verify(token, APP_SECRET);
+        return username;
     }
 
     throw new Error('Not authenticated');
 }
 
-export default getUser;
+export default getUsername;
