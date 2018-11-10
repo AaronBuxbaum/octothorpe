37f2a98b7d119d1696c6da69303ec9a7af1e6993
Aaron Buxbaum <me@aaronbuxbaum.com>
2018-11-09 19:13:15 -0800

    Update profiel

--- server/src/resolvers/mutations/profile/updateProfile.ts
+++ server/src/resolvers/mutations/profile/updateProfile.ts
@@ -1,9 +1,7 @@
 import userModel from '../../../models/userModel';
 
-const updateProfile = async (root, { userInfo, image }, context) => {
+const updateProfile = async (root, { userInfo }, context) => {
     const { user } = context;
-    const img = await image;
-    console.log(img);
     return userModel.findByIdAndUpdate(user, userInfo, { new: true });
 }
 
