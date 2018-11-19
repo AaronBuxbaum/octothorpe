1dac3648e7422b2afb57e93ff9ebdb32fb7d4e0d
Aaron Buxbaum <me@aaronbuxbaum.com>
2018-11-18 23:35:36 -0500

    Fix returned data from flask

--- flask/server.py
+++ flask/server.py
@@ -26,7 +26,7 @@ def find_matches():
                 rating
                 image
                 hashtags {
-                    title
+                    id
                 }
             }
         }
